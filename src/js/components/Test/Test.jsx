1caffcce44996fec25040a698db7af1a797ff032
sammckay10 <samuel@alt-design.net>
2016-10-21 12:41:33 +0100

    First Commit

---
+++ src/js/components/Test/Test.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class TestComponent extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render () {
+        return (
+            <div>
+                <p>Here is a test react component</p>
+            </div>
+        )
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,12 @@
 import Helpers from "./imports/helpers"
 import React from 'react'
 import ReactDOM from 'react-dom'
+import TestComponent from './components/Test/Test.jsx'
 
 // set the helpers class as a window method that everything can access
 window.helpers = new Helpers;
+
+ReactDOM.render(
+    <TestComponent/>,
+    document.querySelector('body')
+)
